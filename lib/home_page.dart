811dae63141ac8840eba5295596e8a13a0cd40f1
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2021-03-11 14:18:33 +0100

    Worked on navigation design

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,9 +11,74 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: MaterialDrawer(currentPage: "Home"),
       body: ListView(
-
+        children: [
+          Container(
+            margin: EdgeInsets.all(8),
+            height: AppBar().preferredSize.height,
+            color: Colors.indigo,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(8),
+                  margin: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Colors.deepOrange),
+                  child: Text(
+                    "Home",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  margin: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Colors.deepOrange),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  margin: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Colors.deepOrange),
+                  child: Text(
+                    "Register",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  margin: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Colors.deepOrange),
+                  child: Text(
+                    "Blog",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  margin: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Colors.deepOrange),
+                  child: Text(
+                    "New Post",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
